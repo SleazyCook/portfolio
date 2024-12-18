0a99c0b303918b2e19814cc9cbd1c0beb62647f3
Andrew Cook <andrewjcook93@gmail.com>
2024-12-18 15:05:16 -0600

    move fetch request to parent file

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import Sidebar from './Sidebar.js'
@@ -7,7 +8,18 @@ import '../../styles/app/sidebar.css'
 import '../../styles/pages/home.css'
 import '../../styles/pages/experience.css'
 
+const URL = 'https://zenquotes.io/api/quotes/[random]'
+
 function App() {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(URL, {mode: 'no-cors'})
+            console.log(result)
+        }
+        fetchData()
+    }, [])
+
     return (
         <div className='app'>
 
--- src/components/components/Quote.js
+++ src/components/components/Quote.js
@@ -1,16 +1,15 @@
-import { useEffect } from 'react'
 
-const URL = 'https://zenquotes.io/api/quotes/[random]'
 
 function Quote() {
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await fetch(URL, {mode: 'no-cors'})
-            console.log(result)
-        }
-        fetchData()
-    }, [])
+    // moved to App.js
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const result = await fetch(URL, {mode: 'no-cors'})
+    //         console.log(result)
+    //     }
+    //     fetchData()
+    // }, [])
 
     return(
         <div className='quote'>
