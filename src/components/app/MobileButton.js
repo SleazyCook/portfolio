2c8a260e97daf0f00b2f08823e479f1e2c6078fa
Andrew Cook <andrewjcook93@gmail.com>
2024-12-19 17:58:23 -0600

    fix mobileBtn showing on desktop

--- src/components/app/MobileButton.js
+++ src/components/app/MobileButton.js
@@ -1,6 +1,30 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function MobileButton() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 1000);
+        };
+
+        // Call the function once on mount to set the initial value
+        handleResize();
+
+        // Add event listener for resize events
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener on unmount
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+  console.log(isMobile)
+
+  if (!isMobile) return null;
+
     return(
         <div className='mobile-btn__container'>
             <Link to='/' className='mobile-btn'>
