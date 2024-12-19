977c8a14bbcd069224d53bbdd3b673559406cb7f
Andrew Cook <andrewjcook93@gmail.com>
2024-12-18 23:31:02 -0600

    basic modal function

--- src/components/pages/experience/ImageGallery.js
+++ src/components/pages/experience/ImageGallery.js
@@ -1,7 +1,70 @@
+import { useState } from 'react'
+
 function ImageGallery({gallery}) {
+
+    const [modalOpen, setModalOpen] = useState(false)
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const openModal = (index) => {
+        setModalOpen(true)
+        setCurrentIndex(index)
+    }
+
+    const closeModal = () => {
+        setModalOpen(false)
+    }
+
+    const nextImage = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % gallery.length)
+    }
+
+    const prevImage = () => {
+        setCurrentIndex((prevIndex) => 
+            prevIndex == 0 ? gallery.length - 1 : prevIndex - 1
+        )
+    }
+
+    const handleCloseModal = (event) => {
+        if (event.target.id === 'detailed-media-parent' || event.target.id === 'modal-close') {
+            closeModal()
+        }
+    }
+
     return(
         <div className='image-gallery'>
-            <img src={gallery[0].img} />
+            {gallery.map((galleryObj, index) => {
+                return(
+                    <img 
+                        onClick={() => openModal(index)}
+                        src={galleryObj.img} 
+                        key={index} 
+                        alt={galleryObj.alt} />
+                )
+            })}
+
+            {/* Modal */}
+            {modalOpen && (
+                <div className="modal" id='detailed-media-parent' onClick={handleCloseModal}>
+                    <div className="modal-content">
+                        <span className="close" onClick={handleCloseModal} id='modal-close'>
+                        &times;
+                        </span>
+                        <div className="modal-image-container">
+                        <button className="prev" onClick={prevImage}>
+                            &lt;
+                        </button>
+                        <img
+                            src={gallery[currentIndex].img}
+                            alt={gallery[currentIndex].alt}
+                            className="modal-image"
+                        />
+                        <button className="next" onClick={nextImage}>
+                            &gt;
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
