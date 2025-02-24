a48c227f55f27f5e03849477f1e9e7e55ebef60a
Andrew Cook <andrewjcook93@gmail.com>
2025-02-24 10:26:32 -0600

    update photography title on interests

--- src/data/interests.js
+++ src/data/interests.js
@@ -1,7 +1,7 @@
 const interests = [
     {
         id: 1,
-        name: 'Hobby Photography',
+        name: 'Digital Photography',
         link: 'https://www.instagram.com/developedbydrewford/',
         img: 'https://i.imgur.com/o3TGB2S.jpg',
         description: 'Learning photography with my Sony ZV1 became a passion and using my Canon RP has only solidified it. Taking daily pictures has helped me discover hidden gems both at home and during my travels, capturing everything from small details to big moments. It’s been amazing to find beauty in the ordinary, whether I’m at home or exploring somewhere new. Each photo is a new opportunity to see the world differently.',
